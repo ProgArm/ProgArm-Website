852db172aa61c2d60550dcf86fb4e5e08ae21afb
Alex Jakimenko <alex.jakimenko@gmail.com>
2014-08-21 04:57:23 +0000

    Oddmuse updated to the latest version

--- cgi-bin/current.pl
+++ cgi-bin/current.pl
@@ -1631,7 +1631,7 @@ sub RcHeader {
   } else {
     $html .= $q->h2((GetParam('days', $RcDefault) != 1)
 		    ? Ts('Updates in the last %s days', $days)
-		    : Ts('Updates in the las %s day',  $days));
+		    : Ts('Updates in the last %s day',  $days));
   }
   my $action = '';
   my ($idOnly, $userOnly, $hostOnly, $clusterOnly, $filterOnly,
@@ -1781,8 +1781,9 @@ sub RcHtml {
   };
   ProcessRcLines($printDailyTear, $printRCLine);
   $html .= '</ul>' if $inlist;
-  my $to = GetParam('from', $Now - GetParam('days', $RcDefault) * 86400);
-  my $from = $to - GetParam('days', $RcDefault) * 86400;
+  # use delta between from and upto, or use days, whichever is available
+  my $to = GetParam('from', GetParam('upto', $Now - GetParam('days') * 86400));
+  my $from = $to - (GetParam('upto') ? GetParam('upto') - GetParam('from') : GetParam('days', $RcDefault) * 86400);
   my $more = "action=rc;from=$from;upto=$to";
   foreach (qw(all showedit rollback rcidonly rcuseronly rchostonly
 	      rcclusteronly rcfilteronly match lang followup)) {
@@ -2291,7 +2292,7 @@ sub GetHtmlHeader {   # always HTML!
   my $base = $SiteBase ? $q->base({-href=>$SiteBase}) : '';
   $base .= '<link rel="alternate" type="application/wiki" title="'
     . T('Edit this page') . '" href="'
-    . ScriptUrl('action=edit;id=' . UrlEncode(GetId())) . '" />' if $id;
+    . ScriptUrl('action=edit;id=' . UrlEncode($id)) . '" />' if $id;
   return $DocumentHeader
     . $q->head($q->title($title) . $base
       . GetCss() . GetRobots() . GetFeeds() . $HtmlHeaders
@@ -2393,7 +2394,7 @@ sub GetFooterLinks {
   if ($id and $rev ne 'history' and $rev ne 'edit') {
     if ($CommentsPattern) {
       if ($id =~ /$CommentsPattern/o) {
-	push(@elements, GetPageLink($1, undef, 'original', T('a')));
+	push(@elements, GetPageLink($1, undef, 'original', T('a'))) if $1;
       } else {
 	push(@elements, GetPageLink($CommentsPrefix . $id, undef, 'comment', T('c')));
       }
@@ -2536,9 +2537,9 @@ sub DoDiff {      # Actualy call the diff program
   RequestLockDir('diff') or return '';
   WriteStringToFile($oldName, $_[0]);
   WriteStringToFile($newName, $_[1]);
-  my $diff_out = `diff \Q$oldName\E \Q$newName\E`;
+  my $diff_out = `diff -- \Q$oldName\E \Q$newName\E`;
   utf8::decode($diff_out); # needs decoding
-  $diff_out =~ s/\\ No newline.*\n//g; # Get rid of common complaint.
+  $diff_out =~ s/\n\K\\ No newline.*\n//g; # Get rid of common complaint.
   ReleaseLockDir('diff');
   # No need to unlink temp files--next diff will just overwrite.
   return $diff_out;
@@ -2848,10 +2849,13 @@ sub RequestLockDir {
 	return 1 if RequestLockDir($name, undef, undef, undef, 1);
       }
       return 0 unless $error;
-      ReportError(Ts('Could not get %s lock', $name) . ": $!. "
-      . Ts('The lock was created %s.', CalcTimeSince($Now - $ts))
-      . ($retried ? ' ' . T('Maybe the user running this script is no longer allowed to remove the lock directory?') : ''),
-      '503 SERVICE UNAVAILABLE');
+      my $unlock = ScriptLink('action=unlock', T('unlock the wiki'), 'unlock');
+      ReportError(Ts('Could not get %s lock', $name) . ": $!. ",
+        '503 SERVICE UNAVAILABLE', undef,
+        Ts('The lock was created %s.', CalcTimeSince($Now - $ts))
+	. ($retried ? ' ' . T('Maybe the user running this script is no longer allowed to remove the lock directory?') : '')
+        . ' ' . qq{Sometimes locks are left behind if a job crashes.}
+	. ' ' . qq{After ten minutes, you could try to $unlock.});
     }
     sleep($wait);
   }
@@ -3697,7 +3701,7 @@ sub MergeRevisions {   # merge change from file2 to file3 into file1
   WriteStringToFile($name2, $file2);
   WriteStringToFile($name3, $file3);
   my ($you, $ancestor, $other) = (T('you'), T('ancestor'), T('other'));
-  my $output = `diff3 -m -L \Q$you\E -L \Q$ancestor\E -L \Q$other\E \Q$name1\E \Q$name2\E \Q$name3\E`;
+  my $output = `diff3 -m -L \Q$you\E -L \Q$ancestor\E -L \Q$other\E -- \Q$name1\E \Q$name2\E \Q$name3\E`;
   ReleaseLockDir('merge'); # don't unlink temp files--next merge will just overwrite.
   return $output;
 }
